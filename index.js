37a963f3ecca5229530b5152a163f92b82ef26e4
Adam Charron <adam@charrondev.com>
2018-01-05 16:21:57 -0500

    Fix environmental check

--- index.js
+++ index.js
@@ -4,28 +4,20 @@
  */
 
 const envTargets = {
-    browsers: [
-        "ie > 9",
-        "last 6 iOS versions",
-        "last 4 versions"
-    ]
-}
+    browsers: ["ie > 9", "last 6 iOS versions", "last 4 versions"]
+};
 
-console.log(process.env.NODE_ENV);
-
-module.exports = {
-    presets: [
-        require('babel-preset-react'),
-        require('babel-preset-env'),
-    ],
+const preset = {
+    presets: [require("babel-preset-react"), require("babel-preset-env")],
     plugins: [
-        require('babel-plugin-transform-object-rest-spread'),
-        require('babel-plugin-syntax-dynamic-import'),
-        require('babel-plugin-transform-class-properties'),
-    ],
-    env: {
-        test: {
-            plugins: [require("babel-plugin-dynamic-import-node").default] // https://github.com/airbnb/babel-plugin-dynamic-import-node/issues/27
-        }
-    }
+        require("babel-plugin-transform-object-rest-spread"),
+        require("babel-plugin-syntax-dynamic-import"),
+        require("babel-plugin-transform-class-properties")
+    ]
+};
+
+if (process.env.NODE_ENV === "test") {
+    preset.plugins.push(require("babel-plugin-dynamic-import-node").default);
 }
+
+module.exports = preset;
