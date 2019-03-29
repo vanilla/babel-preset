81bbb72790559691928b661dd192b06ff9deb131
Adam Charron <adam@charrondev.com>
2019-03-29 09:27:36 -0400

    Update babel versions & add env options

--- index.js
+++ index.js
@@ -16,14 +16,22 @@ const runtimePlugins = process.env.NODE_ENV === "test"
         ],
     ];
 
+const envOptions = {
+    useBuiltIns: false,
+}
+
+if (
+    process.env.NODE_ENV = "production" ||
+    process.env.DEV_COMPAT === "compat"
+) {
+    envOptions.targets: "ie > 10, last 4 versions";
+}
+
 const preset = {
     presets: [
         [
             resolve("@babel/preset-env"),
-            {
-                useBuiltIns: false,
-                targets: "ie > 10, last 4 versions"
-            }
+            envOptions,
         ],
         resolve("@babel/preset-react"),
         resolve("@babel/preset-typescript"),
